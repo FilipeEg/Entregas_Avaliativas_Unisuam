4b26d76d897ff13a2fc862761c01ed9a2a0756c8
FilipeEg <filipemarriel@souunisuam.com.br>
2025-09-07 12:23:05 -0300

    Add files via upload

---
+++ cartao_apresentacao/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'info',
+    loadComponent: () => import('./page/info/info.page').then( m => m.InfoPage),
+  },
+];
